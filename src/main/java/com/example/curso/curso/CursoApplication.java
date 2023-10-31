a44922820e3bb9fc4e02ce1916a686777fe5c620
Ruyllex <ruymori630@gmail.com>
2023-10-31 20:26:13 -0300

    base de datos

--- src/main/java/com/example/curso/curso/CursoApplication.java
+++ src/main/java/com/example/curso/curso/CursoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursoApplication {
+public class CursoApplication extends ServletInitializer{
 	public String PORT = System.getenv("PORT");
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);
